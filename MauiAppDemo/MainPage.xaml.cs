5b33856242d44443693630623cb188325cb63458
Arafat Zaman Anik <arafat.zaman.anik@gmail.com>
2024-07-27 14:50:55 +0600

    Add project files.

---
+++ MauiAppDemo/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Maui.Controls;
+namespace MauiAppDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Text = "Clicked!";
+        }
+    }
+}
